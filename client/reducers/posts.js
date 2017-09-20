f68236f66df9f2633a8c2c3a4ad84b03fd8fc794
kyle <kdeak1210@gmail.com>
2017-09-20 19:53:32 -0400

    Reducer for posts incrementing likes returns the new state
    Its easiest to do this using the spread operator. Added a .babelrc file so that it is correctly transpiled (was missing file)

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -5,8 +5,24 @@
 
 // default param empty array bc first time run state is undefined
 function posts(state = [], action){
-  console.log(state, action);
-  return state;
+  
+  switch(action.type){
+    
+    case 'INCREMENT_LIKES':
+    // return the updated state
+      console.log('Incrementing Likes');
+      const i = action.index;
+      return [
+        ...state.slice(0,i), // before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1), // after the one we are updating        
+      ]
+      break;
+    
+    default:
+      // All the reducers always run; make sure to include default
+      return state;
+  }
 }
 
 export default posts;
